25493ba902bf379f788dec593b9715f126a35489
chunyukuo88 <alexgochenour@gmail.com>
2020-01-25 19:01:25 -0500

    Automated the loading of Capital Vue 2.0 website, entering in a stock ticker symbol, clicking the search button, and clicking the resulting company's official website.

--- src/main/java/capitalVue/CapitalVue.java
+++ src/main/java/capitalVue/CapitalVue.java
@@ -1,6 +1,8 @@
 package capitalVue;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CapitalVue {
 
@@ -12,8 +14,32 @@ public class CapitalVue {
 		driver.get("https://chunyukuo88.github.io/company-stats/");
 	}
 	
+	public void getSearchBox() {
+		WebElement searchBox = driver.findElement(By.id("input"));
+		searchBox.sendKeys("MMM"); //Stock symbol for the 3M company
+	}
+	
+	public void clickButton() {
+		WebElement button = driver.findElement(By.id("button"));
+		button.click();
+	}
+	
+	public void clickCompanyWebsite() {
+		WebElement officialSite = driver.findElement(By.linkText("http://www.3m.com")); //URL for company site
+		officialSite.click();
+	}
+	
 	public static void main(String[] args) {
 		CapitalVue obj = new CapitalVue();
 		obj.launchBrowser();
+		obj.getSearchBox();
+		obj.clickButton();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		obj.clickCompanyWebsite();
 	}
 }
